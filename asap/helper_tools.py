02f141efde46c5c062600ef882ef767cc38536a6
Dionco <cobelens@strw.leidenuniv.nl>
2026-02-05 11:37:45 +0100

    Removed improved configure function

--- asap/helper_tools.py
+++ asap/helper_tools.py
@@ -16,23 +16,3 @@ def configure():
             f.close()
             print("File config.ini created")
             file.close()
-
-def configure_improved(config_path):
-    import os
-    import importlib.resources
-
-    filename = os.path.join(config_path, 'config.ini')
-
-    if os.path.isfile(filename):
-        print(f"File {filename} already exists")
-    else:
-        # Access the file as a resource
-        with importlib.resources.open_text("asap.resources", "config.ini") as file:
-            g = file.read()
-            
-            f = open(filename, 'w')
-            f.write(g)
-
-            f.close()
-            print(f"File {filename} created")
-            file.close()
