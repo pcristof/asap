301f72cb78f6b33a25434e0b67817e236efbc6b1
Dionco <cobelens@strw.leidenuniv.nl>
2026-04-15 12:09:22 +0200

    asap: optimize convolution process in broaden_spectrum_2_cy function

--- asap/c_tools/effects.pyx
+++ asap/c_tools/effects.pyx
@@ -388,11 +388,10 @@ def broaden_spectrum_2_cy(
         pad_right = len_master - 1 - pad_left
         padded = np.empty(n + pad_left + pad_right, dtype=np.float64)
         pad_1d(oflux, padded, pad_left, pad_right, 1.0)
-        padded_conv = np.convolve(padded, masterkernel, mode='same')
-        # Extract the middle n elements (removing the padding)
-        output[:] = padded_conv[pad_left:pad_left+n]
+        padded = np.convolve(padded, masterkernel, mode='same')
+        output[:] = padded#[pad_left:pad_left+n]
     else:
-        output[:] = np.convolve(oflux, masterkernel, mode='same')
+        output[:] = np.convolve(oflux, masterkernel, mode='same')#[(len_master-1)//2:(len_master-1)//2+n]
 
     return output
 
